af4853fd0f2d398bac0cf441b09233bcd98c65f4
mkuzdowicz <martin.kuzdowicz@gmail.com>
2021-11-06 16:55:40 +0000

    wip

--- src/space-stretch-2.js
+++ src/space-stretch-2.js
@@ -2,32 +2,6 @@ import Phaser from "phaser";
 import ballPath from './vendor/assets/images/asteroid1.png'
 import shipPath from './vendor/assets/images/ship-rotated.png'
 
-const isMobile = window.innerWidth < 450
-const scaleDownSketch = !isMobile
-
-const config = {
-    type: Phaser.AUTO,
-    parent: 'main-canvas',
-    // width: scaleDownSketch ? window.innerWidth / 1.2 : window.innerWidth,
-    // height: scaleDownSketch ? window.innerHeight / 1.3 : window.innerHeight / 1.2,
-    width: 1024,
-    height: 768,
-    scene: {
-        preload,
-        create,
-        update,
-    },
-    pixelArt: true,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-        }
-    }
-};
-
-const game = new Phaser.Game(config);
-
 let player1, cursors;
 let score = 0
 let scoreBoard
@@ -83,37 +57,55 @@ function create() {
 
     function collectBalls(avatar, ball) {
         ball.destroy()
+        score += 1
+        scoreBoard.setText(`Score: ${score}`)
     }
 }
 
 const paddleSpeedUp = 50
 const ballSpeedNg = 45
 function update(time, delta) {
-
     player1.body.setAngularVelocity(0);
     player1.body.setVelocity(0, 0);
     player1.body.setAcceleration(0)
 
-    // manage events for neck stretches
     if (window.gameUpMove()) {
         const ng = player1.angle
         const vec = this.physics.velocityFromAngle(ng, paddleSpeedUp)
-        // const vx = (Math.cos(player1.rotation)- (Math.PI/2)) * 50
-        // const vy = (Math.sin(player1.rotation)- (Math.PI/2)) * 50
         player1.body.setVelocity(vec.x, vec.y);
-
     } else if (window.gameJumpMove() || cursors.up.isDown) {
         const ng = player1.angle
         const vec = this.physics.velocityFromAngle(ng, paddleSpeedUp)
-        // const vx = (Math.cos(player1.rotation)- (Math.PI/2)) * 50
-        // const vy = (Math.sin(player1.rotation)- (Math.PI/2)) * 50
         player1.body.setVelocity(vec.x, vec.y);
-
-        // this.physics.velocityFromRotation(player1.rotation, 200,
-        //     player1.body.velocity)
     } else if (window.gameLeftMove() || cursors.left.isDown) {
         player1.body.setAngularVelocity(ballSpeedNg * -1);
     } else if (window.gameRightMove() || cursors.right.isDown) {
         player1.body.setAngularVelocity(ballSpeedNg);
     }
 }
+
+const isMobile = window.innerWidth < 450
+const scaleDownSketch = !isMobile
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'main-canvas',
+    // width: scaleDownSketch ? window.innerWidth / 1.2 : window.innerWidth,
+    // height: scaleDownSketch ? window.innerHeight / 1.3 : window.innerHeight / 1.2,
+    width: 1024,
+    height: 768,
+    scene: {
+        preload,
+        create,
+        update,
+    },
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+        }
+    }
+};
+
+const game = new Phaser.Game(config)
